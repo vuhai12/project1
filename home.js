28914425f91a2011c86b4062e9701d9b4df8eca4
vuhai12 <>
2021-11-17 09:29:08 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,100 @@
+
+$(document).ready(function() {
+  var fixHeight = function() {
+    $('.navbar-nav').css(
+      'max-height',
+      document.documentElement.clientHeight - 150
+    );
+  };
+  fixHeight();
+  $(window).resize(function() {
+    fixHeight();
+  });
+  $('.navbar .navbar-toggler').on('click', function() {
+    fixHeight();
+  });
+  $('.navbar-toggler, .overlay').on('click', function() {
+    $('.mobileMenu, .overlay').toggleClass('open');
+  });
+});
+
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("menuid-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#menu-leader-left1').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 1500,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
+
